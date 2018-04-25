fa0cddf5607e79e189490c01cb838f800f682aa0
Maria Alice de Melo Sousa <alice.melosousa@gmail.com>
2018-04-25 06:45:48 -0300

    Update main.cpp

--- Projeto1/main.cpp
+++ Projeto1/main.cpp
@@ -5,10 +5,30 @@
 #include "retangulo.h"
 using namespace std;
 
+Point centro(float x, float y, float largura, float altura);
 
 int main()
 {
-    Retangulo ret(5,5,5,5);
+    Retangulo retangulo(0,0,4,3);
+    Point cm;
+    retangulo.imprime();
+
+    cm = centro(0,0,3,4);
+    centro.imprime();
+
+    cout << "Area: " << retangulo.area()<<endl;
+
+    cout << "Transladar em (-3,4): ";
+    ret.translada(-3,4);
+    ret.imprime();
+
+    cout << "Area depois da translacao: " << ret.area()<<endl;
+
+    cout<< "Rotacao "<<endl;
+    ret.rotaciona(M_PI/6, cm);
+    ret.imprime();
+    
+    /* Retangulo ret(5,5,5,5);
     Point p1;
 
     cout << "Quantidade de Vertices: "<< ret.getNumPoints() <<endl<<endl;
@@ -27,11 +47,21 @@ int main()
     ret.imprime();
     cout  << "Apos a rotacao "<<endl;
     ret.rotaciona(M_PI, p1);
-    ret.imprime();
+    ret.imprime(); */
 }
 
 
+Point centro(float x, float y, float largura, float altura){
+    Point cm(0,0);
 
+    float coordenada1, coordenada2;
+
+    coordenada2= (2*x-altura)/2;
+    coordenada1= (2*y+largura)/2;
+
+    cm.setXY(coordenada1,coordenada2);
+    return cm;
+}
 
 
 
